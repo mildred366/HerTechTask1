3f41363a39342a7ae661578b0df1dc7833643f24
Mildred Erubore <derubore@gmail.com>
2023-03-30 17:25:25 +0100

    Added the eventlistener

---
+++ script.js
@@ -0,0 +1,47 @@
+const change = document.getElementById("imagetext");
+
+change.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("imagetext2");
+
+change2.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("imagetext3");
+
+change3.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("imagetext4");
+
+change4.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("imagetext5");
+
+change5.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("imagetext6");
+
+change6.addEventListener('click', function (event) {
+
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
